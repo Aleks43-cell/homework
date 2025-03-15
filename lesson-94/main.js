9c57cc2053d00fa50d01eec0a3024fac868f64a9
Aleks43-cell <lucin6477@gmail.com>
2025-03-15 13:57:31 +0300

    Add files via upload

---
+++ lesson-94/main.js
@@ -0,0 +1,12 @@
+const firstName = 'Alexey';
+const lastName = 'Luchin';
+const age = '21';
+
+const city = 'Volgograd';
+const birthDate = '2003';
+
+
+console.log(firstName, lastName, age + ' years old');
+console.log('My hometown: ' +  city);
+console.log('Date of birth: ' + birthDate);
+
