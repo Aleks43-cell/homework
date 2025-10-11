c0fdd7d6dca9c46700765b99365f13bdb02704a0
Aleks43-cell <lucin6477@gmail.com>
2025-10-11 22:59:17 +0300

    js

---
+++ lesson-89/js/main.js
@@ -0,0 +1,20 @@
+import {getTodosFromLocalStorage} from "./storage.js";
+import {renderTodos, initTodoHandlers} from "./dom.js";
+
+
+
+const todos = getTodosFromLocalStorage() || [];
+renderTodos(todos);
+initTodoHandlers(todos);
+
+
+
+
+
+
+
+
+
+
+
+
